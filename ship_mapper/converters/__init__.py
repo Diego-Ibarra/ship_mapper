14327a9f67a8823a09468e6034c6518d33e44d08
Diego Ibarra <Diego.Ibarra@dal.ca>
2018-03-18 14:45:23 -0300

    Added capability to import custom "converters"

--- ship_mapper/converters/__init__.py
+++ ship_mapper/converters/__init__.py
@@ -1,15 +1,20 @@
-
-def convert_to_nc(data_in, data_out, converter):
-    # Import appropiate converter (which is file in folder "converters")
+def convert_to_nc(data_in, data_out, converter, path=None):
+    '''
+     Import appropiate converter, which is a file in folder "converters" within
+     this module, or a customade file in the "path" folder 
+     '''
     try:
-        exec('from . import ' + converter)
-    except ImportError as e:
-        errorMessage = ("converter '" + converter + "' does not exist. "
+        if path is None:
+            exec('from . import ' + converter)
+        else:
+            import imp
+            file, pathname, description = imp.find_module(converter,[path])
+            exec(converter + " = imp.load_module('"+ converter +"', file, pathname, description)")
+    except ImportError:
+        errorMessage = ("converter '" + converter + "' does not exist in this 'path'. "
                         'Check spelling. Otherwise, you may need to make a new '
-                        'converter file (e.g. MyFile.py) and save it in '
-                        './converters')
+                        'converter file (e.g. MyPath/MyConverterFile.py)')
         raise ImportError(errorMessage)
-        return
     
     # call converter function from approatie converter file
     eval(converter + '.convert(data_in, data_out)')
