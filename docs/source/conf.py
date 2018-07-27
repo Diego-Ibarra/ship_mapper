3a848573fb547a8496c4c22e063bd8f3486b9ea4
Diego Ibarra <Diego.Ibarra@dal.ca>
2018-07-27 15:02:38 -0300

    Added matplotlib to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,6 +19,8 @@ sys.path.insert(0, os.path.abspath('../../ship_mapper'))
 #This adds all subdirectories under the specified directory (e.g. ship_mapper)
 for root, dirName, files in os.walk('../../ship_mapper'):
     sys.path.insert(0, os.path.abspath(root))
+    
+autodoc_mock_imports = ['matplotlib']
 
 
 # -- Project information -----------------------------------------------------
