aee05d2ba85dc23532acfb2519afaab927b7b368
Diego Ibarra <Diego.Ibarra@dal.ca>
2018-08-14 15:11:10 -0300

    test

--- examples/projects/settings.py
+++ examples/projects/settings.py
@@ -8,6 +8,6 @@ GRIDS = Path(r'C:\Users\IbarraD\ship_mapper\examples\grids')
 
 DATA = Path(r'C:\Users\IbarraD\ship_mapper\examples\data')
 
-ORACLE_USER = 'test_user' 
+ORACLE_USER = '1' 
 
-ORACLE_PASSWORD = 'test_password'
+ORACLE_PASSWORD = '1'
